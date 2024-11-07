49d7eeb7f8fe3dbe169fe8f596bb7f2e86c17f91
HP <HP@DESKTOP-CO09DFI>
2024-11-07 16:14:57 +0700

    Add project files.

---
+++ SIMTHE/SIMTHE.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SIMTHE
+{
+    partial class SIMTHE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(316, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH SACH THE SIM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(135, 118);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(540, 184);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID Sim";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Số thẻ sim";
+            this.columnHeader2.Width = 90;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ID Nhà mạng";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Ngày kích hoạt";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Ngày Hết hạn";
+            this.columnHeader5.Width = 120;
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThoat.Location = new System.Drawing.Point(361, 337);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(75, 23);
+            this.buttonThoat.TabIndex = 2;
+            this.buttonThoat.Text = "Thoat";
+            this.buttonThoat.UseVisualStyleBackColor = true;
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // SIMTHE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonThoat);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "SIMTHE";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SIMTHE_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button buttonThoat;
+    }
+}
+
---
+++ SIMTHE/SIMTHE.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SIMTHE
+{
+    public partial class SIMTHE : Form
+    {
+        listView1.Sorting = SortOrder.Ascending;
+        String connectstring = @"Data Source=DESKTOP-CO09DFI\MSSQLSERVER01;Initial Catalog=simthe;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader doc;
+        string sql;
+        int i = 0;
+        public SIMTHE()
+        {
+            InitializeComponent();
+        }
+        private void hienthi()
+        {
+            listView1.Items.Clear();
+            listView1.View = View.Details;
+            conn.Open();
+            sql = @"select * from sim";
+            cmd = new SqlCommand(sql, conn);
+            doc = cmd.ExecuteReader();
+            i = 0;
+            while (doc.Read())
+            {
+                listView1.Items.Add(doc[0].ToString());
+                listView1.Items[i].SubItems.Add(doc[1].ToString());
+                listView1.Items[i].SubItems.Add(doc[2].ToString());
+                listView1.Items[i].SubItems.Add(doc[3].ToString());
+                listView1.Items[i].SubItems.Add(doc[4].ToString());
+                i++;
+            }
+            conn.Close();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SIMTHE_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectstring);
+            hienthi();
+        }
+
+        private void buttonThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+           
+        }
+    }
+}
